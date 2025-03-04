864ac60b7ac56d19ee87357a627cca975fc22030
m9zjl <i667zjl@hotmail.com>
2025-03-04 23:36:02 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,37 @@
-use indoc::indoc;
-use rust_web::ThreadPool;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // 监听地址: 127.0.0.1:7878
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    let pool = ThreadPool::new(6);
-
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        pool.execute(|| handle_connection(stream));
+        match stream {
+            Ok(stream) => {
+                handle_connection(stream);
+            }
+            Err(_e) => {}
+        }
     }
 }
 
-fn handle_connection(mut tcp_stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut tcp_stream);
-    let _http_request = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect::<Vec<String>>();
+fn handle_connection(mut stream: TcpStream) {
+    let mut buff = [0; 512];
 
-    // println!("Request: {:#?}", http_request);
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = indoc! {"
-            <!DOCTYPE html>
-            <html lang=\"en\">
-            <head>
-                <meta charset=\"utf-8\">
-                <title>Hello!</title>
-            </head>
-            <body>
-            <h1>Hello!</h1>
-            <p>Hi from Rust</p>
-            </body>
-            </html>
-    "};
-    let length = contents.len();
+    loop {
+        match stream.read(&mut buff) {
+            Ok(size) if size != 0 => {
+                let response = "+PONG\r\n";
+                stream.write(response.as_bytes()).unwrap();
+                stream.flush().unwrap();
+            }
+            Ok(_) => {}
+            Err(_e) => {}
+        }
+    }
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    tcp_stream.write_all(response.as_bytes()).unwrap();
+    stream.read(&mut buff).unwrap();
+
+    print!("Received: {}", String::from_utf8_lossy(&buff[..]));
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
 }
